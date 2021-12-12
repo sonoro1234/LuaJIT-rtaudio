0a80f698a8e7188c95725d71ec63665935a39b0c
Victor Bombi <sonoroster@gmail.com>
2021-12-12 12:06:50 +0100

    examples: no_gui and imgui.window

---
+++ samples/no_gui.lua
@@ -0,0 +1,69 @@
+------sleep function
+local ffi = require("ffi")
+ffi.cdef[[
+void Sleep(int ms);
+int poll(struct pollfd *fds, unsigned long nfds, int timeout);
+]]
+
+local sleep
+if ffi.os == "Windows" then
+  function sleep(s)
+    ffi.C.Sleep(s*1000)
+  end
+else
+  function sleep(s)
+    ffi.C.poll(nil, 0, s*1000)
+  end
+end
+------------ get devices
+local rt = require"rtaudio_ffi"
+RtAudioInfo = rt.GetAllInfo()
+local API 
+local device 
+--check first good api-default device
+for i=1,#RtAudioInfo.APIS do
+	API = RtAudioInfo.APIS[i]
+	device = RtAudioInfo.API[API].default_output
+	print("test",API,device)
+	if RtAudioInfo.API[API].devices[device].probed then
+		break
+	end
+end
+------------------
+
+print("using",API,"device",device)
+local api = rt.compiled_api_by_name(API)
+local dac = rt.create(api)
+
+--copy specs from file
+local sndf = require"sndfile_ffi"
+local AudioPlayer = require"rtAudioPlayer"
+local filename = "african_roomS.wav";
+local info = sndf.get_info(filename)
+local audioplayer,err = AudioPlayer({
+    dac = dac,
+    device = dac:get_default_output_device(),
+    freq = info.samplerate, 
+    format = rt.FORMAT_FLOAT32,
+    channels = info.channels, 
+    samples = 1024})
+
+assert(audioplayer,err)
+--insert several files
+for i=1,10 do
+    --filename, level, timeoffset
+    audioplayer:insert(filename,(11-i)*0.1,i*0.6)
+end
+--show them
+for node in audioplayer:nodes() do
+    print("node",node.sf)
+end
+print"return to start"
+io.read"*l"
+--play them 7 secs
+audioplayer:start()
+local err = rt.error(dac)
+print("start error",ffi.string(err~=nil and err or "no error"))
+sleep(7);
+--close
+audioplayer:close()
